b0b0bbf0b4af58b3134541c4809ddca930de3448
Pranav13-09 <morepranav51@gmail.com>
2023-10-14 18:54:29 +0530

    Navbar Changes

--- Components/NavBar2.js
+++ Components/NavBar2.js
@@ -16,7 +16,7 @@ const Header = () => {
            <div className='md:flex items-center justify-between  py-4 md:px-10 px-7'>
           
             <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-                <BookOpenIcon className='w-7 h-7  text-white'/>
+             
                 <span className='text-white'> EduMentor</span>
             </div>
             
@@ -29,7 +29,7 @@ const Header = () => {
             <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-blue-700 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
                 {
                     Links.map((link,index) => (
-                        <li key={index } className='md:ml-8 md:my-0 my-7 font-semibold'>
+                        <li key={index} className='md:ml-8 md:my-0 my-7 font-semibold'>
                         <Link href={link.link} className='text-white '>{link.name}</Link>
                     </li>))
                 }
